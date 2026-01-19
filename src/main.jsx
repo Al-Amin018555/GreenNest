0a1f4e9cd67dd988fae4091f7d6ece1376d33ada
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-01-19 21:34:03 +0600

    Initial setup: Vite + Tailwind + DaisyUI + React Router

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import "./index.css";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-5xl bg-purple-200">Hello World</div>,
+  },
+]);
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />,
+);
