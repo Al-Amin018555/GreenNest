ed9f49e0d66d0a57d1c01729a5c2dc4e26943c5f
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-01-30 19:41:49 +0600

    navbar made sitcky

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         }
     </>
     return (
-        <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar sticky top-0 z-1 bg-base-100 shadow-sm">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -57,7 +57,7 @@ const Navbar = () => {
 
                     <ul
                         tabIndex="-1"
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow">
                         <li>
                             <a className="justify-between">
                                 Profile
