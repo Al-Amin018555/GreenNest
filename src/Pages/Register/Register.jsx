ed8e7a61f652d7b0cf065296c0b9693ea4415641
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-01-31 09:52:27 +0600

    implemented password validation

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router";
 import { AuthContext } from "../../context/AuthContext";
 import { useContext } from "react";
+import toast from "react-hot-toast";
 
 const Register = () => {
     const navigate = useNavigate();
@@ -12,14 +13,16 @@ const Register = () => {
     const onSubmit = (data) => {
         console.log(data);
         const { email, password, name, photo } = data;
+
         createUser(email, password)
             .then(() => {
                 console.log("user successfully created");
                 profileUpdate(name, photo)
                 navigate('/')
             })
-
-
+            .catch(error => {
+               toast.error(error.message);
+            });
     };
     const handleGoogleLogin = () => {
         googleLogin()
@@ -87,17 +90,36 @@ const Register = () => {
                                     className="input"
                                     placeholder="Password"
                                     name="password"
-                                    {...register("password", { required: true })} />
-                                {errors.password && <span className="text-red-600">This field is required</span>}
+                                    {...register("password", { 
+                                        required: "Password is required",
+                                        minLength: {
+                                            value: 6,
+                                            message: "Password must be at least 6 characters"
+                                        },
+                                        validate: (value) => {
+                                            if (!/^(?=.*[A-Z])/.test(value)) {
+                                                return "Password must contain at least one uppercase letter";
+                                            }
+                                            if (!/^(?=.*[a-z])/.test(value)) {
+                                                return "Password must contain at least one lowercase letter";
+                                            }
+                                            if (!/^(?=.*[0-9])/.test(value)) {
+                                                return "Password must contain at least one number";
+                                            }
+                                            return true;
+                                        }
+                                    })} 
+                                />
+                                {errors.password && <span className="text-red-600">{errors.password.message}</span>}
 
                                 <div className="flex">
                                     <p>Alread have an account?</p>
                                     <Link to='/login' className="link text-blue-500 link-hover">Login</Link>
                                 </div>
                                 <button className="btn btn-neutral mt-4">Register</button>
-                                <button onClick={handleGoogleLogin} className="btn mt-2 bg-white text-black border-[#e5e5e5]">
+                                <button type="button" onClick={handleGoogleLogin} className="btn mt-2 bg-white text-black border-[#e5e5e5] w-full">
                                     <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
-                                    Login with Google
+                                    Register with Google
                                 </button>
                             </fieldset>
                         </div>
