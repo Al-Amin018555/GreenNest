81b22c4bedf0a5cbbfa01abb704cb26a6f0bfb40
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-02-07 21:29:12 +0600

    login and register description added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -65,8 +65,7 @@ const Login = () => {
                     <div className="text-center lg:text-left">
                         <h1 className="text-5xl font-bold">Login now!</h1>
                         <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
+                            Log in to your GreenNest account to explore premium indoor plants, access detailed care guides, and manage your profile with ease.
                         </p>
                     </div>
                     <form onSubmit={handleSubmit(onSubmit)} className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -42,8 +42,7 @@ const Register = () => {
                     <div className="text-center lg:text-left">
                         <h1 className="text-5xl font-bold">Register now!</h1>
                         <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
+                            Register for GreenNest to explore beautiful indoor plants, learn how to care for them, and manage everything from your personal profile.
                         </p>
                     </div>
                     <form onSubmit={handleSubmit(onSubmit)} className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
